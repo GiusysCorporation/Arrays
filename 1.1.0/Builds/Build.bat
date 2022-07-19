183da15b1f96c3aab6031cd5f21bae81854b9411
Giulio <giulio.vittoria@gmail.com>
2022-07-19 22:16:27 +0200

    Commiting stable release v1.1.0.

---
+++ 1.1.0/Builds/Build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Compiling into objects...
+cd Objects
+
+g++ -c ../../Source/FixedArray.cpp -o FixedArray.o
+g++ -c ../../Source/DynamicArray.cpp -o DynamicArray.o
+
+echo Linking objects dynamicly...
+g++ -shared FixedArray.o DynamicArray.o -o ../Dynamic/Arrays.dll -L./../../Libs/Builds/Dynamic -lTypes
+
+echo Linking objects staticly...
+ar rcs ../Static/Arrays.lib FixedArray.o DynamicArray.o
+
+cd ..
